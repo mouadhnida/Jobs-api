37918316b251ce0545d460cfad5c1ba1efc121e2
Mouad <104643026+mouadhnida@users.noreply.github.com>
2023-06-07 02:00:13 +0100

    create the react app

---
+++ server/app.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+require('express-async-errors');
+
+
+
+
+
+const express = require('express');
+const app = express();
+
+const authRouter = require('./routes/auth');
+const jobsRouter = require('./routes/jobs');
+
+// error handler
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(express.json());
+
+
+
+// connect to db
+const connectDB = require('./db/connect');
+const authenticateUser = require('./middleware/authentication')
+
+// routes
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', authenticateUser, jobsRouter)
+
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3001;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start()
