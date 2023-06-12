092c01c7755d0224d86c61f27350f7574319e03e
Mouad <104643026+mouadhnida@users.noreply.github.com>
2023-06-13 00:36:35 +0100

    added dashboard logic

--- server/controllers/jobs.js
+++ server/controllers/jobs.js
@@ -1,6 +1,8 @@
 const Job = require("../models/Job");
 const StatusCodes = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
+const mongoose = require("mongoose");
+const moment = require("moment")
 
 const getAllJobs = async (req, res) => {
   const { search, status, jobType, sort } = req.query;
@@ -21,10 +23,8 @@ const getAllJobs = async (req, res) => {
   }
 
 
-
   let result = Job.find(queryObject);
 
- 
 
   if (sort === "latest") {
     result = result.sort("-createdAt");
@@ -109,6 +109,52 @@ const deleteJob = async (req, res) => {
   }
   res.status(StatusCodes.OK).send();
 };
+
+const showStats = async (req, res) => {
+    let stats = await Job.aggregate([
+      { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+      { $group: { _id: '$status', count: { $sum: 1 } } },
+    ]);
+  
+    stats = stats.reduce((acc, curr) => {
+      const { _id: title, count } = curr;
+      acc[title] = count;
+      return acc;
+    }, {});
+  
+    const defaultStats = {
+      pending: stats.pending || 0,
+      interview: stats.interview || 0,
+      declined: stats.declined || 0,
+    };
+  
+    let monthlyApplications = await Job.aggregate([
+      { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+      {
+        $group: {
+          _id: { year: { $year: '$createdAt' }, month: { $month: '$createdAt' } },
+          count: { $sum: 1 },
+        },
+      },
+      { $sort: { '_id.year': -1, '_id.month': -1 } },
+      { $limit: 6 },
+    ]);
+  
+    monthlyApplications = monthlyApplications.map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
+      const date = moment()
+        .month(month - 1)
+        .year(year)
+        .format('MMM Y');
+      return { date, count };
+    })
+    .reverse()
+  
+    res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
+  };
 
 module.exports = {
   getAllJobs,
