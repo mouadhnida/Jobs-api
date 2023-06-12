fb70e5926ac22b97763fc519cbd63ede59614981
Mouad <104643026+mouadhnida@users.noreply.github.com>
2023-06-12 11:39:57 +0100

    added update user

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -38,6 +38,32 @@ const login = async (req, res) => {
       },})
 }
 
+const updateUser = async (req, res) => {
+    const { email, name, lastName, location } = req.body;
+    if (!email || !name || !lastName || !location) {
+      throw new BadRequest('Please provide all values');
+    }
+    const user = await User.findOne({ _id: req.user.userId });
+  
+    user.email = email;
+    user.name = name;
+    user.lastName = lastName;
+    user.location = location;
+  
+    await user.save();
+    const token = user.createJWT();
+    res.status(StatusCodes.OK).json({
+      user: {
+        email: user.email,
+        lastName: user.lastName,
+        location: user.location,
+        name: user.name,
+        token,
+      },
+    });
+  };
+  
+
 module.exports = {
     register,
     login
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
+const authenticateUser = require('../middleware/authentication')
 
 const {register, login} = require('../controllers/auth')
 
 router.post('/register', register)
-router.post ('/login', login)
+router.post ('/login', authenticateUser ,login)
 
 module.exports = router
