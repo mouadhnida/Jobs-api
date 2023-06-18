7b3fe05088e2d859c01d50155971fd128928db2d
Mouad <104643026+mouadhnida@users.noreply.github.com>
2023-06-18 19:33:18 +0100

    simplify the code

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -6,34 +6,26 @@ import { BiAddToQueue } from "react-icons/bi";
 import { ImProfile } from "react-icons/im";
 
 function Sidebar() {
+  const lis = [{name:"Stats", link: "/", icon: <IoStatsChart/> },
+               {name:"All Job", link: "/all-jobs", icon: <FaSearch/>}, 
+               {name:"Add job", link: "/add-job", icon: <BiAddToQueue/>}, 
+               {name:"Profile", link: "/profile", icon: <ImProfile/> }]
   return (
     <ul className="w-full mt-28 text-slate-500 h-60 text-lg flex flex-col justify-between">
-      <li className="group/sidebar h-1/4"><Link to="/">
-        <div className="flex items-center w-full h-full pl-10 group-hover/sidebar:bg-slate-100 group-hover/sidebar:pl-14 transition-all duration-500">
-          <IoStatsChart className="mr-4 w-6 h-6 group-hover/sidebar:text-main transition-all duration-500" />
-          <span className="group-hover/sidebar:text-black"> Stats</span>
-        </div></Link>
-      </li>
-      <li className="group/sidebar h-1/4"><Link to="/all-jobs">
-        <div className="flex items-center w-full h-full pl-10 group-hover/sidebar:bg-slate-100 group-hover/sidebar:pl-14 transition-all duration-500">
-          <FaSearch className="mr-4 w-6 h-6 group-hover/sidebar:text-main transition-all duration-500" />
-          <span className="group-hover/sidebar:text-black"> All Jobs</span>
-        </div></Link>
-      </li>
-      <li className="group/sidebar h-1/4"><Link to="/add-job">
-        <div className="flex items-center w-full h-full pl-10 group-hover/sidebar:bg-slate-100 group-hover/sidebar:pl-14 transition-all duration-500">
-          <BiAddToQueue className="mr-4 w-6 h-6 group-hover/sidebar:text-main transition-all duration-500" />
-          <span className="group-hover/sidebar:text-black"> Add Job</span>
-        </div></Link>
-      </li>
-      <li className="group/sidebar h-1/4"><Link to="/profile">
-        <div className="flex items-center w-full h-full pl-10 group-hover/sidebar:bg-slate-100 group-hover/sidebar:pl-14 transition-all duration-500">
-          <ImProfile className="mr-4 w-6 h-6 group-hover/sidebar:text-main transition-all duration-500" />
-          <span className="group-hover/sidebar:text-black"> Profile</span>
-        </div></Link>
-      </li>
+      {lis.map((li, i) => {
+        return (
+        <li className="group/sidebar h-1/4" key={i}>
+        <Link to={li.link}>
+          <div className="flex items-center w-full h-full pl-10 group-hover/sidebar:bg-slate-100 group-hover/sidebar:pl-14 transition-all duration-500">
+            <div className="mr-4 w-6 h-6 group-hover/sidebar:text-main transition-all duration-500">{li.icon}</div>
+            <span className="group-hover/sidebar:text-black">{li.name}</span>
+          </div>
+        </Link>
+        </li>)
+      }) 
+      }
     </ul>
-  );
+  )
 }
 
 export default Sidebar;
