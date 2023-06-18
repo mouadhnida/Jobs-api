5e90388b5bdb0bab0e08ab9fe257299a18c78d5f
Mouad <104643026+mouadhnida@users.noreply.github.com>
2023-06-18 19:19:47 +0100

    added links to sidebar

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -7,30 +7,30 @@ import { ImProfile } from "react-icons/im";
 
 function Sidebar() {
   return (
-    <ul className="w-full mt-32 text-slate-500 h-60 text-lg flex flex-col justify-between">
-      <li className="group/sidebar h-1/4">
+    <ul className="w-full mt-28 text-slate-500 h-60 text-lg flex flex-col justify-between">
+      <li className="group/sidebar h-1/4"><Link to="/">
         <div className="flex items-center w-full h-full pl-10 group-hover/sidebar:bg-slate-100 group-hover/sidebar:pl-14 transition-all duration-500">
           <IoStatsChart className="mr-4 w-6 h-6 group-hover/sidebar:text-main transition-all duration-500" />
           <span className="group-hover/sidebar:text-black"> Stats</span>
-        </div>
+        </div></Link>
       </li>
-      <li className="group/sidebar h-1/4">
+      <li className="group/sidebar h-1/4"><Link to="/all-jobs">
         <div className="flex items-center w-full h-full pl-10 group-hover/sidebar:bg-slate-100 group-hover/sidebar:pl-14 transition-all duration-500">
           <FaSearch className="mr-4 w-6 h-6 group-hover/sidebar:text-main transition-all duration-500" />
           <span className="group-hover/sidebar:text-black"> All Jobs</span>
-        </div>
+        </div></Link>
       </li>
-      <li className="group/sidebar h-1/4">
+      <li className="group/sidebar h-1/4"><Link to="/add-job">
         <div className="flex items-center w-full h-full pl-10 group-hover/sidebar:bg-slate-100 group-hover/sidebar:pl-14 transition-all duration-500">
           <BiAddToQueue className="mr-4 w-6 h-6 group-hover/sidebar:text-main transition-all duration-500" />
           <span className="group-hover/sidebar:text-black"> Add Job</span>
-        </div>
+        </div></Link>
       </li>
-      <li className="group/sidebar h-1/4">
+      <li className="group/sidebar h-1/4"><Link to="/profile">
         <div className="flex items-center w-full h-full pl-10 group-hover/sidebar:bg-slate-100 group-hover/sidebar:pl-14 transition-all duration-500">
           <ImProfile className="mr-4 w-6 h-6 group-hover/sidebar:text-main transition-all duration-500" />
           <span className="group-hover/sidebar:text-black"> Profile</span>
-        </div>
+        </div></Link>
       </li>
     </ul>
   );
