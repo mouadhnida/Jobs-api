70f81c1b8c48476313808117f796539945fffe15
Mouad <104643026+mouadhnida@users.noreply.github.com>
2023-07-10 22:58:34 +0100

    added cors

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ require("express-async-errors");
 
 const helmet = require("helmet");
 const xss = require("xss-clean");
+const cors = require("cors");
 
 const express = require("express");
 const app = express();
@@ -16,7 +17,7 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
 app.use(helmet());
-
+app.use(cors())
 app.use(xss());
 
 // connect to db
